d564fe5e1184e47e91ac901d6c4ff16bd55b036f
sugata2002 <rupsrc009@gmail.com>
2024-04-14 20:15:55 +0100

    menu bar change ui

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -2,7 +2,7 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule  } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCoffee , faArrowRight , faBars, fas , faX} from '@fortawesome/free-solid-svg-icons';
+import { faCoffee , faArrowRight , faBars, fas , faX , faHome , faCircleInfo , faArrowDown} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-landingpage',
   standalone: true,
@@ -14,7 +14,10 @@ export class LandingpageComponent {
   faCoffee = faCoffee;
   faArrowRight = faArrowRight;
   faCoffees = [fas, faCoffee];
+  fahome= faHome
   fabars= faBars;
+  faarrowdown = faArrowDown
+  facircleinfo = faCircleInfo
   fax = faX;
   isIconToggled: boolean = true;
 
